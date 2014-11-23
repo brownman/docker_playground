02e57d6ad38c9fb671f36abc25887a208a38d499
Ubuntu <linnoroot@meanio-runner.meanio-runner.a5.internal.cloudapp.net>
2014-11-23 16:31:40 +0000

    [fix] - it is working now :|

---
+++ SH/run_from_gitlab-ci-runner.sh
@@ -0,0 +1,5 @@
+#SERVER_URL=''
+#REGISTRATION_TOKEN=''
+#exec -c
+ bundle exec ./bin/setup
+
--- SH/setup_runners.sh
+++ SH/setup_runners.sh
@@ -2,12 +2,12 @@ echo Setup runners
 echo Create the CI runner user and clone the gitlab-ci-runner repository:
 
 sudo gem install bundler
-sudo adduser --disabled-login --gecos 'GitLab CI Runner' gitlab_ci_runner
-echo ERR?
-echo something looks wrong here.. one has to exit the sub-shell in order to continue executing this script
+sudo Adduser --disabled-login --gecos 'GitLab CI Runner' gitlab_ci_runner
+echo  we are different user now - who is we ?
 
 sudo su gitlab_ci_runner
 cd ~/
+echo this is a nice new home for a user like me
 git clone https://gitlab.com/gitlab-org/gitlab-ci-runner.git
 cd gitlab-ci-runner
 echo Install the gems for the runner:
