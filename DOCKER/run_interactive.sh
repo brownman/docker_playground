29aac62a4c8252346295064519aa5f2978a56714
brownman <brownman556@gmail.com>
2014-11-28 17:27:20 +0200

    Update run_interactive.sh

--- DOCKER/run_interactive.sh
+++ DOCKER/run_interactive.sh
@@ -6,3 +6,22 @@ sudo   docker run  \
     -e HOME=/root \
     -e GITLAB_SERVER_FQDN=git.mean.io \
     brownman/gitlab-ci-runner-nodejs
+# Based on https://github.com/gitlabhq/gitlab-ci-runner/blob/master/Dockerfile
+# by Sytse Sijbrandij <sytse@gitlab.com>
+
+# This script will start a runner in a docker container.
+#
+# First build the container and give a name to the resulting image:
+# docker build -t codingforce/gitlab-ci-runner-nodejs github.com/bkw/gitlab-ci-runner-nodejs
+#
+# Then set the environment variables and run the gitlab-ci-runner in the container:
+# docker run -e CI_SERVER_URL=https://ci.example.com -e REGISTRATION_TOKEN=replaceme -e HOME=/root -e GITLAB_SERVER_FQDN=gitlab.example.com codingforce/gitlab-ci-runner-nodejs
+#
+# After you start the runner you can send it to the background with ctrl-z
+# The new runner should show up in the GitLab CI interface on /runners
+#
+# You can start an interactive session to test new commands with:
+# docker run -e CI_SERVER_URL=https://ci.example.com -e REGISTRATION_TOKEN=replaceme -e HOME=/root -i -t codingforce/gitlab-ci-runner-nejs:latest /bin/bash
+#od
+# If you ever want to freshly rebuild the runner please use:
+# docker build -no-cache -t codingforce/gitlab-ci-runner-nodejs github.com/bkw/gitlab-ci-runner-nodejs
