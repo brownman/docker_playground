08f2f0d56de3a6b4c50c5281ac01a2352f38ad50
brownman <brownman556@gmail.com>
2014-12-21 14:10:58 +0200

    Create circle.sh

---
+++ circle.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+################# anchor
+dir_root=$( cd `dirname $0`; echo $PWD )
+echo 1>&2 "[dir_root] $dir_root"
+
+permit(){
+  chmod +x *.sh . -R
+}
+
+set_env(){
+  permit
+}
+test_all(){
+  ./test/linking.sh
+}
+
+
+steps(){
+  set_env
+  test_all
+}
+
+
+steps
