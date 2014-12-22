18b501a016ee2406f5f38b9f698bf3bc64fcfa22
brownman <brownman556@gmail.com>
2014-12-22 11:39:12 +0200

    Update wrap.sh

--- example/wrap.sh
+++ example/wrap.sh
@@ -1,1 +1,3 @@
-commander 'docker run -i Dockerfile bash < test.sh'
+#https://docs.docker.com/reference/commandline/cli/
+commander docker build -t docker_current .
+commander 'docker run -i docker_current bash < test.sh'
