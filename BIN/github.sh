d1fb1736f6f25f7e8dad386a3ab130fdfd8b1638
brownman <brownman556@gmail.com>
2015-02-12 20:52:34 +0200

    touch

--- BIN/github.sh
+++ BIN/github.sh
@@ -7,7 +7,7 @@ git config --local user.name $username
 git config --local user.email $email
 git add .
 git commit  -am "'$@'"
-echo After doing this, you may fix the identity used for this commit with:
+#echo After doing this, you may fix the identity used for this commit with:
 
-    git commit --amend --reset-author
+#    git commit --amend --reset-author
 
