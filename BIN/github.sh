2a081ddf4e7e2517b6c7f58c84f111419e2cec9b
root <root@docker0.linnovate.net>
2015-02-12 16:46:44 +0200

    touch

--- BIN/github.sh
+++ BIN/github.sh
@@ -1,8 +1,8 @@
-source ~/config.cfg    
+#source ~/config.cfg    
 git config --global user.name $username
     git config --global user.email $email
 
 echo After doing this, you may fix the identity used for this commit with:
 
-    git commit --amend --reset-author
+    echo git commit --amend --reset-author
 
