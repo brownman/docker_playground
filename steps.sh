6695dedb95c77e3a766a0facd2f46342072cc113
brownman <brownman556@gmail.com>
2015-02-15 16:08:22 +0200

    use repo: json_wire_protocol

--- steps.sh
+++ steps.sh
@@ -4,7 +4,8 @@ set -e
 export dir_root=$PWD
 source $dir_root/source.cfg
 ./DOCKER/kill.sh
+test -d /extra || { sudo mkdir -p /extra; cd extra; git clone https://github.com/brownman/json_wire_protocol.git; }
 run_selenium
 sleep 1
-#run_tests
-run_webapp_deamon
+run_tests
+#run_webapp_deamon
