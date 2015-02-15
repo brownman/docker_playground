be0519d5d96756ea52ea3e976986b0912ec43656
brownman <brownman556@gmail.com>
2015-02-15 16:42:01 +0200

    Update steps.sh

--- steps.sh
+++ steps.sh
@@ -4,7 +4,6 @@ set -e
 export dir_root=$PWD
 source $dir_root/source.cfg
 ./DOCKER/kill.sh
-test -d /extra || { echo 'enable repo on /extra'; sudo mkdir -p /extra; git clone --depth 1 https://github.com/brownman/json_wire_protocol.git /extra/json_wire_protocol; } 
 run_selenium
 sleep 1
 run_tests
