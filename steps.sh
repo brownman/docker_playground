f905d93842b4823507d73819ec81eae0587f2d41
brownman <brownman556@gmail.com>
2015-03-03 18:24:56 +0200

    [steps.sh] fix mongo err - need volume: /data/db

--- steps.sh
+++ steps.sh
@@ -2,8 +2,8 @@
 set -u
 set -e
 export dir_root=$PWD
-file_src='../gitlab_linnovate.cfg'
-test -f $file_src && { source $file_src; } || true
+file_src='../gitlab_linnovate.conf'
+test -f $file_src && { source $file_src; } || { echo file not found: $file_src; sleep 5; true; }
 source $dir_root/source.cfg
 commander ./SH/DOCKER/kill.sh
 commander run_mongo
