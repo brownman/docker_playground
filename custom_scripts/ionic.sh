2be1b882011b885f530ed80b01ec45ff4fda6cc3
brownman <brownman556@gmail.com>
2015-03-03 17:34:15 +0200

    [test container] docker for ionic [steps.sh] run deamon of mongo container [vars.cfg] - fix naming of var: flags_X

---
+++ custom_scripts/ionic.sh
@@ -0,0 +1,41 @@
+export dir_project=$PWD
+export dir_home=$HOME
+
+#ls -l $dir_project
+#
+
+
+source ~/.profile
+source ~/.bashrc
+source ~/.nvm/nvm.sh
+#ls $HOME
+#cat ~/config.cfg
+source ~/config.cfg
+set +u
+commander nvm use 0.10
+#commander whereis npm
+#commander which npm
+#commander npm -v
+#commander env
+#commander netstat -ntlp
+commander ps fax
+#commander 'env | grep 4444'
+#commander env
+
+#commander ls -l /extra/json_wire_protocol
+#commander cat /extra/json_wire_protocol/docker/remote.sh
+
+#commander cd /extra/json_wire_protocol
+#commander export dir_root=$PWD
+
+#commander sudo chmod +x /extra/json_wire_protocol/docker.sh
+#commander bash -c /extra/json_wire_protocol/docker.sh $file_test
+
+
+commander "ls -l $( npm -g root )"
+
+cd $dir_project
+commander npm install -g
+commander npm link
+commander gulp
+
--- steps.sh
+++ steps.sh
@@ -2,10 +2,13 @@
 set -u
 set -e
 export dir_root=$PWD
+file_src='../gitlab_linnovate.cfg'
+test -f $file_src && { source $file_src; } || true
 source $dir_root/source.cfg
-./SH/DOCKER/kill.sh
-run_selenium
+commander ./SH/DOCKER/kill.sh
+commander run_mongo
+commander run_selenium
 sleep 1
 #run_tests
-run_webapp_deamon
-./SH/DOCKER/attach.sh
+commander run_webapp_deamon
+commander ./SH/DOCKER/attach.sh
