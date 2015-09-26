6341d5426fd98eb0281bb2064086f6c56fc4b3ed
brownman <brownman@linnovate.net>
2015-09-26 17:29:49 +0300

    containers: +drupal +mysql

--- SH/DOCKER/kill.sh
+++ SH/DOCKER/kill.sh
@@ -1,6 +1,12 @@
-str="$( docker ps | awk '{print $1}' |  tail -n +2 )"
-echo "str: $str"
-for item in $str;do
-commander "docker kill $item"
-done
+    sudo docker ps -a | awk '{print $1}' | grep -v CONTAINER | xargs -n1 -I {} docker stop {}
+    #sudo docker ps -a | awk '{print $1}' | grep -v CONTAINER | xargs -n1 -I {} docker kill {}
+
+    sudo docker ps -a | awk '{print $1}' | grep -v CONTAINER | xargs -n1 -I {} docker rm {}
+
+
+# str="$( docker ps | awk '{print $1}' |  tail -n +2 )"
+# echo "str: $str"
+# for item in $str;do
+# commander "docker kill $item"
+# done
 
--- kill.sh
+++ kill.sh
@@ -2,18 +2,9 @@
 set -u
 set -e
 export dir_root=$PWD
-file_src='../gitlab_linnovate.conf'
-test -f $file_src && { source $file_src; } || { echo file not found: $file_src; sleep 5; true; }
-file_src='../config.cfg'
-test -f $file_src && { source $file_src; } || { echo file not found: $file_src; sleep 5; true; }
 source $dir_root/source.cfg
+    sudo docker ps -a | awk '{print $1}' | grep -v CONTAINER | xargs -n1 -I {} docker stop {}
+
+    sudo docker ps -a | awk '{print $1}' | grep -v CONTAINER | xargs -n1 -I {} docker rm {}
+
 commander ./SH/DOCKER/kill.sh
-exit
-commander run_mongo
-#commander run_selenium
-#
-./selenium/selenium_hub.sh
-sleep 1
-run_tests && {
-commander run_webapp_deamon
-commander ./SH/DOCKER/attach.sh; }
