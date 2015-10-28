b243e643b6915355fa2b5b1bf75148481c48e136
brownman <brownman556@gmail.com>
2015-10-28 16:51:31 +0200

    touch

--- SH/DOCKER/rm_images.sh
+++ SH/DOCKER/rm_images.sh
@@ -6,12 +6,13 @@ rm_not_used(){
 #docker images -q |xargs docker rmi
 docker images | grep '' | awk '{print $3}' | xargs docker rmi
 }
+
 rm_all_images(){
 local num=${1:-2}
 echo skip first columns
-docker rmi <<<  docker images | tail  -n +${num} | awk '{ print $3; }'
-#cat /tmp/images | tail -n  +4 | awk '{print $3}' | xargs docker rmi
+docker rmi <<<  docker images | tail  -n +"${num}" | awk '{ print $3; }'
 }
+
 
 rm_not_used
 rm_not_tagged
